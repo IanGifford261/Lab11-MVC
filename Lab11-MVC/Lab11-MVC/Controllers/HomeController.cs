2170d71b69edecb1b9959189742fb1e8e550c221
iangifford <lightyagamie10@gmail.com>
2019-04-02 11:14:07 -0700

    building out controller

---
+++ Lab11-MVC/Lab11-MVC/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab11_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index()
+        {
+
+        }
+
+        public IActionResult Results()
+        {
+
+        }
+
+    }
+}
